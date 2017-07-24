0e9763d2b17cb1daad4fbc6bc2eebaa834395b9b
TheOnlyArtz <gamesil456@gmail.com>
2017-07-24 19:40:29 +0300

    set up logger completely

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
 const config = require('./config/config.json');
 const client = new Discord.Client()
+const winstonLogger = require('./classes/logger.js')
+
+const winstonClass = new winstonLogger
+global.logger = winstonClass.logger
 
 client.on('ready', () => {
-  logger.info('Music bot is ready!')
+  logger.info(`MusicBot is ready!`);
+  logger.verbose(`Connected as ${client.user.tag}`)
+  logger.verbose(`With the ID of ${client.user.id}`)
 })
 
 client.login(config.token)
