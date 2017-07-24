1b6c2b96e94eac8ad6f0f5a7ddd2900a79a46288
Amit <callcraft456@gmail.com>
2017-07-24 19:37:04 +0300

    logger 2
    Wrote the class

---
+++ classes/logger.js
@@ -0,0 +1,20 @@
+const moment = require('moment');
+
+class winstonLogger {
+    constructor() {
+        this.winston = require('winston');
+        this.transport = new (this.winston.transports.Console)({
+            level: 'debug',
+            colorize: true,
+            timestamp: true,
+            timestamp() {
+                return moment(new Date).format('h:mm:ss a');
+            }
+        });
+        this.logger = new (this.winston.Logger)({
+            transports: [this.transport]
+        });
+    }
+  }
+
+module.exports = winstonLogger;
