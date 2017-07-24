2578796b25e37a367f2239da58bc2ef22da239e8
TheOnlyArtz <gamesil456@gmail.com>
2017-07-24 20:34:03 +0300

    Getting into fetching the song, downloading it and join into a VC

--- commands/play.js
+++ commands/play.js
@@ -1,8 +1,19 @@
 const fetch = require('snekfetch');
 const ytdl = require('ytdl-core');
-
-exports.run = async (client, msg) => {
+const fs = require('fs')
+exports.run = async (client, message) => {
   const toPlay = message.content.split(' ').slice(1).join(' ');
+  fs.readdir('./songs/', async (err, files) => {
+    console.log(files.length);
+    ytdl(toPlay, { filter: function(format) { return format.container === 'mp4'; } })
+    .pipe(fs.createWriteStream(`./songs/Song${files.length + 1}.mp4`));
+    if (!message.guild.member(message.author).voiceChannel) {
+      logger.error('Please join into a voice channel.')
+    } else {
+      message.guild.member(message.author).voiceChannel.join();
+    }
+    console.log();
+  })
 };
 
 module.exports.help = {
