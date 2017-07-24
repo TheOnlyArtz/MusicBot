12201720a421f7600156ba6353d057a937ecd2fd
TheOnlyArtz <gamesil456@gmail.com>
2017-07-24 21:15:30 +0300

    Using a method for info

--- commands/play.js
+++ commands/play.js
@@ -10,7 +10,13 @@ exports.run = async (client, message) => {
   message.guild.member(message.author).voiceChannel.join()
  .then(connection => {
    const stream = ytdl(toPlay, { filter : 'audioonly' });
+   ytdl.getInfo(toPlay, async (err, info) => {
+     if (err) logger.log(err);
+     console.log(info);
+   })
    const dispatcher = connection.playStream(stream, streamOptions);
+  //  logger.info(dispatcher.range);
+
  })
  .catch(console.error);
 
