7171b9aa97db793bfdd0b8b102e356bfa0ec54e9
Amit <callcraft456@gmail.com>
2017-07-24 20:08:31 +0300

    Semi
    semi

--- commands/play.js
+++ commands/play.js
@@ -2,7 +2,7 @@ const fetch = require('snekfetch');
 const ytdl = require('ytdl-core');
 
 exports.run = async (client, msg) => {
-  const toPlay = message.content.split(' ').slice(1).join(' ')
+  const toPlay = message.content.split(' ').slice(1).join(' ');
 };
 
 module.exports.help = {
