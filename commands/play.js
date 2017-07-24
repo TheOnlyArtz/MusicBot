e772b5720af8abe6b302fc5983e6a927b3c276e2
TheOnlyArtz <gamesil456@gmail.com>
2017-07-24 20:00:39 +0300

    Checking command handler

--- commands/play.js
+++ commands/play.js
@@ -2,9 +2,9 @@ const fetch = require('snekfetch');
 const ytdl = require('ytdl-core');
 
 module.exports = async (client, msg) => {
-
+  msg.channel.send('s')
 };
 
 module.exports.help = {
-  name : 'play'  
+  name : 'play'
 }
