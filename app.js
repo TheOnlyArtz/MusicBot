5575305e3a7cb66ce539d3b9cb29143a95d7f75d
TheOnlyArtz <gamesil456@gmail.com>
2017-07-26 21:31:42 +0300

    Telling statistics

--- app.js
+++ app.js
@@ -1,69 +1,97 @@
 const Discord = require('discord.js');
 const config = require('./config/config.json');
-const client = new Discord.Client()
-const winstonLogger = require('./classes/logger.js')
-const fs = require('fs')
-const chalk = require('chalk')
-const db = require('node-json-db')
-const queue = new db("./commands/songs.json", true, true)
 
-const winstonClass = new winstonLogger
-global.logger = winstonClass.logger
+const client = new Discord.Client();
+const winstonLogger = require('./classes/logger.js');
+const fs = require('fs');
+const chalk = require('chalk');
+const db = require('node-json-db');
+
+const queue = new db('./commands/songs.json', true, true);
+const songAmount = [];
+const winstonClass = new winstonLogger();
+global.logger = winstonClass.logger;
 
 client.commands = new Discord.Collection();
 
-client.on('ready', () => {
-  logger.info(`MusicBot is ready!`);
-  logger.verbose(`Connected as ${client.user.tag}`)
-  logger.verbose(`With the ID of ${client.user.id}`)
-  queue.delete('/parent')
-})
+function getGuilds() {
+	const map = client.guilds.map(i => i.id);
+	const guildMaps = [];
+  let songMaps = [];
+  try {
+    map.forEach(index => {
+      if (queue.getData(`/parent/${index}/TheSongs/mySongs/queue`)) {
+        songMaps.push(index);
+      }
+      logger.verbose(`Deleting all the songs inside all the queues... ${maps.length}`);
+    })
+  } catch (e) {
+
+  }
+  try {
+    map.forEach(index => {
+      if (queue.getData(`/parent/${index}`)) {
+        guildMaps.push(index);
+      }
+      logger.verbose(`Deleting ${guildMaps.length} Guilds and ${songMaps.length} Songs from the queue`);
+    })
+  } catch (e) {
+  }
+}
+client.on('ready', async () => {
+	logger.info(`MusicBot is ready!`);
+	logger.verbose(`Connected as ${client.user.tag}`);
+	logger.verbose(`With the ID of ${client.user.id}`);
+	logger.info('======================================');
+  await getGuilds()
+	queue.delete('/parent');
+});
 
 process.on('unhaldedRejection', (reason, p) => {
-  logger.error(reason)
+	logger.error(reason);
 });
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) logger.error(err);
-  let jsfiles = files.filter(f => f.split(".").pop() === "js");
-  if (jsfiles.length <= 0) {
-    logger.info("No commands to load!");
-    return;
-  }
-  logger.info(`Loading ${jsfiles.length} commands!`);
+fs.readdir('./commands/', (err, files) => {
+	if (err) {
+		logger.error(err);
+	}
+	const jsfiles = files.filter(f => f.split('.').pop() === 'js');
+	if (jsfiles.length <= 0) {
+		logger.info('No commands to load!');
+		return;
+	}
+	logger.info(`Loading ${jsfiles.length} commands!`);
 
-
-  //Loop through all the files and load them
-  jsfiles.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    logger.info(`${i + 1}: ${f} loaded`);
-    client.commands.set(props.help.name, props)
-  });
+  // Loop through all the files and load them
+	jsfiles.forEach((f, i) => {
+		const props = require(`./commands/${f}`);
+		logger.info(`${i + 1}: ${f} loaded`);
+		client.commands.set(props.help.name, props);
+	});
 });
 
+const prefix = '.';
+client.on('message', async message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pinging...').then(r => {
+			r.edit(`Ping! ${r.createdTimestamp - message.createdTimestamp}ms`);
+		});
+	}
+	const args = message.content.split(' ').slice(1);
+	const command = message.content.split(' ')[0];
+	if (!command.startsWith(prefix)) {
+		return;
+	}
+	const cmd = client.commands.get(command.slice(prefix.length));
+// Runs the command handler
+	if (cmd) {
+		try {
+			cmd.run(client, message, args);
+			logger.info(`${chalk.cyan(cmd.help.name)} just been executed by ${chalk.magenta(message.author.username)} inside ${chalk.yellow(message.guild.name)}`);
+		} catch (e) {
+			logger.error(e);
+		}
+	}
+});
 
-
-
-
-let prefix = '.'
-client.on('message', async (message) => {
-  if (message.content.startsWith(`${prefix}ping`)) {
-    message.channel.send('Pinging...').then(r => {
-      r.edit(`Ping! ${r.createdTimestamp - message.createdTimestamp}ms`)
-    })
-  }
-  let args = message.content.split(" ").slice(1);
-  let command = message.content.split(" ")[0];
-  if (!command.startsWith(prefix)) return;
-  let cmd = client.commands.get(command.slice(prefix.length));
-//runs the command handler
-  if (cmd)
-    try {
-      cmd.run(client, message, args);
-      logger.info(`${chalk.cyan(cmd.help.name)} just been executed by ${chalk.magenta(message.author.username)} inside ${chalk.yellow(message.guild.name)}`)
-    } catch (e) {
-      logger.error(e);
-    }
-})
-
-client.login(config.token)
+client.login(config.token);
--- commands/music.js
+++ commands/music.js
@@ -3,139 +3,162 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 const config = require('../config/config.json');
 const fetchVideoInfo = require('youtube-info');
-const Discord = require('discord.js')
-const moment = require('moment')
-const db = require('node-json-db')
-const queue = new db("./commands/songs.json", true, true)
+const Discord = require('discord.js');
+const moment = require('moment');
+const db = require('node-json-db');
+
+const queue = new db('./commands/songs.json', true, true);
 const titleForFinal = [];
-const chalk = require('chalk')
+const chalk = require('chalk');
+
 const skipper = [];
-let skipReq = 0
+const skipReq = 0;
 
 exports.run = async (client, message) => {
-  let action = message.content.split(' ')[1]
-  // guilds = {};
-  message.delete()
-  if (action === 'play') {
-    const toPlay = message.content.split(' ').slice(2).join(' ');
-    if (!toPlay) return message.reply('Please add a link of the song to the command')
+	const action = message.content.split(' ')[1];
+  // Guilds = {};
+	message.delete();
+	if (action === 'play') {
+		const toPlay = message.content.split(' ').slice(2).join(' ');
+		if (!toPlay) {
+			return message.reply('Please add a link of the song to the command');
+		}
 
-
-    if(!message.member.voiceChannel) {
-      return message.channel.send('Please get into a voice channel')
-    }
-    if (!toPlay.includes('&list') && !toPlay.includes('index')) {
-      fetch.get(`https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=` + encodeURIComponent(toPlay) + "&key=" + config.ytKey)
-     .then(async (r) => {
-       if (r.body.items[0]) {
-         fetchVideoInfo(`${r.body.items[0].id.videoId}`).then( function (l) {
-           titleForFinal.push(l.title)
-           const embed = new Discord.RichEmbed()
+		if (!message.member.voiceChannel) {
+			return message.channel.send('Please get into a voice channel');
+		}
+		if (!toPlay.includes('&list') && !toPlay.includes('index')) {
+			fetch.get(`https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=` + encodeURIComponent(toPlay) + '&key=' + config.ytKey)
+     .then(async r => {
+	if (r.body.items[0]) {
+		fetchVideoInfo(`${r.body.items[0].id.videoId}`).then(l => {
+			titleForFinal.push(l.title);
+			const embed = new Discord.RichEmbed()
            .setAuthor(`Requested by ${message.author.username} and added to the queue`, l.thumbnailUrl)
            .addField(`Song Info`, `**Owner:** ${l.owner}\n\
     **Date Published:** ${l.datePublished}\n\
     **Duration:** ${(l.duration / 60).toFixed(2)} Minutes\n\
     **Views:** ${l.views}\n\
     **Song Name:** ${l.title}`)
-           .setThumbnail(l.thumbnailUrl)
-           message.channel.send({embed})
-         });
-       }
+           .setThumbnail(l.thumbnailUrl);
+			message.channel.send({embed});
+		});
+	}
 
-       try {
-         queue.getData(`/parent/${message.guild.id}`)
-       } catch (e) {
-         queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: []}, false);
+	try {
+		queue.getData(`/parent/${message.guild.id}`);
+	} catch (e) {
+		queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: []}, false);
+	}
 
-       }
+	queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: [r.body.items[0].id.videoId]}, false);
 
-       queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: [r.body.items[0].id.videoId]}, false);
-
-
-      if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(async (connection) => {
-        logger.info(`Started to stream ${chalk.magenta(titleForFinal)} for ${message.author.username}`)
-        play(connection, message);
-        });
-
-      })
+	if (!message.guild.voiceConnection) {
+		message.member.voiceChannel.join().then(async connection => {
+			logger.info(`Started to stream ${chalk.magenta(titleForFinal)} for ${message.author.username}`);
+			play(connection, message);
+		});
+	}
+})
       .catch(e => {
-        message.reply('We could\' find the requested song :pensive:')
-        logger.error(e)
-      })
-    } else {
-      console.log('got playlist before download');
-      await playLists(message, toPlay)
-      console.log('got playlist after download');
-    }
-  }
-  if (action === 'skip') {
-    if (skipper.indexOf(message.author.id) === -1) {
-      skipper.push(message.author.id);
-      skipReq++;
-      if (skipReq >= Math.ceil((message.member.voiceChannel.members.size - 1) / 2)) {
-        await skip_song(message)
-        skipReq = 0;
-        skipper = [];
-        message.reply('Skipping successfully')
-      } else {
-        message.reply('Your skip counted and you need more ' + Math.ceil(((message.member.voiceChannel.members.size - 1) / 2) - skipReq) + ' Guy(s) to skip the current song')
-      }
-      }
-    }
+	message.reply('We could\' find the requested song :pensive:');
+	logger.error(e);
+});
+		} else {
+			console.log('got playlist before download');
+			await playLists(message, toPlay);
+			console.log('got playlist after download');
+		}
+	}
+	if (action === 'skip') {
+    // If (skipper.indexOf(message.author.id) === -1) {
+      // skipper.push(message.author.id);
+      // skipReq++;
+      // if (skipReq >= Math.ceil((message.member.voiceChannel.members.size - 1) / 2)) {
+        // message.member.voiceChannel.join().then(async (connection) => {
+        //  connection.dispatcher.end()
+        //  logger.info(`${message.author.username} skipped successfully`)
+        //  message.reply('Skipping successfully')
+        //  })
+        //  .catch(e => {
+        //    message.channel.send(`${message.author.username} failed horribly at skipping a song.`)
+        //    logger.error(e)
+        //  })
+
+		await skip_song();
+          // Let list = queue.getData(`/parent/315129822571528193/TheSongs/mySongs`);
+          // if (list.queue) {
+          //   play(list.queue[0])
+          // } else {
+          //   skipper = [];
+          //   skipReq = 0;
+          // }
+	}
+};
+
+function play(connection, message) {
+	const songsQueue = [];
+    // Try {
+    //   queue.getData(`/${message.guild.id}`)
+    // } catch (e) {
+    //   logger.error(e)
+    // }
+	const json = queue.getData(`/parent/${message.guild.id}/TheSongs/mySongs/queue`);
+	dispatcher = connection.playStream(ytdl(json[0], {filter: 'audioonly'}));
+
+	setTimeout(() => {
+		queue.delete((`/parent/${message.guild.id}/TheSongs/mySongs/queue[0]`));
+	}, 3000);
+
+    // Queue.delete(`/${message.guild.id}/queue[0]`)
+
+	if (!message.guild.voiceConnection) {
+		message.member.voiceChannel.join().then(async connection => {
+			logger.info(`Started to stream ${chalk.magenta(titleForFinal)} for ${message.author.username}`);
+			play(connection, message);
+		});
+	}
+
+	const list = queue.getData(`/parent/${message.guild.id}/TheSongs/mySongs/queue[0]`);
+	dispatcher.on('end', () => {
+		if (list) {
+			play(connection, message);
+		} else {
+			connection.disconnect();
+			queue.delete(`/parent/`);
+		}
+	});
 }
 
-  function play(connection, message) {
-    let songsQueue = [];
-    let json = queue.getData(`/parent/${message.guild.id}/TheSongs/mySongs/queue`);
-    dispatcher = connection.playStream(ytdl(json[0], {filter: 'audioonly'}));
-
-
-    setTimeout(() => {
-      queue.delete((`/parent/${message.guild.id}/TheSongs/mySongs/queue[0]`));
-    }, 3000)
-
-
-    let list = queue.getData(`/parent/${message.guild.id}/TheSongs/mySongs/queue[0]`);
-    dispatcher.on('end', function () {
-      if (list) {
-      play(connection, message)
-      } else {
-        connection.disconnect()
-        queue.delete(`/parent/`)
-      }
-    })
-  }
-
-  function playLists(message, id) {
-    fetch.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=' + id.split('&list=')[1] + '&key=' + config.ytKey)
-
+function playLists(message, id) {
+	fetch.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=' + id.split('&list=')[1] + '&key=' + config.ytKey)
     .then(res => {
-      try {
-        queue.getData(`/parent/${message.guild.id}`)
-      } catch (e) {
-        queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: []}, false);
-
-      }
-      res.body.items.forEach(i => {
-        if (i.id) {
-          queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: [i.snippet.resourceId.videoId]}, false);
-        }
-      })
-      if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(async (connection) => {
-        logger.info(`Started by streaming a playlist requested by ${message.author.username}`)
-        play(connection, message);
-        });
-    })
+	try {
+		queue.getData(`/parent/${message.guild.id}`);
+	} catch (e) {
+		queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: []}, false);
+	}
+	res.body.items.forEach(i => {
+		if (i.id) {
+			queue.push(`/parent/${message.guild.id}/TheSongs/mySongs`, {queue: [i.snippet.resourceId.videoId]}, false);
+		}
+	});
+	if (!message.guild.voiceConnection) {
+		message.member.voiceChannel.join().then(async connection => {
+			logger.info(`Started to stream by playing playlist requested by ${message.author.username}`);
+			play(connection, message);
+		});
+	}
+})
     .catch(e => {
-      logger.error(e)
-      logger.error(id.split('&list=')[1])
-    })
-  }
+	logger.error(e);
+	logger.error(id.split('&list=')[1]);
+});
+}
 
-  function skip_song(message) {
-    console.log(dispatcher.end());
-    message.channel.send('Skipping the current song')
+function skip_song() {
+	console.log(dispatcher.end());
 }
 module.exports.help = {
-  name : 'music'
-}
+	name: 'music'
+};
